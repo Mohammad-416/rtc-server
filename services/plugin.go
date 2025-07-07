b73fe614ed481c354eee4f26d2adf26687c443d6
Mohammad-416 <149521106+Mohammad-416@users.noreply.github.com>
2025-07-08 04:58:42 +0530

    Created method for initializing a project repo

--- services/plugin.go
+++ services/plugin.go
@@ -2,6 +2,7 @@ package services
 
 import (
 	"app/urtc/db"
+	"bytes"
 	"encoding/json"
 	"fmt"
 	"net/http"
@@ -31,6 +32,18 @@ func PushProject(w http.ResponseWriter, r *http.Request) {
 		projectModel := &db.ProjectModel{
 			DB: db.DB,
 		}
+
+		tokenModel := &db.TokenModel{
+			DB: db.DB,
+		}
+
+		token, err := tokenModel.GetToken(user.USERNAME)
+		if err != nil {
+			fmt.Println("Error Fetching Token : ", err)
+			w.WriteHeader(http.StatusBadRequest)
+			return
+		}
+
 		//Check if a project by this name already exists
 		project, err := projectModel.GetProjectByName(user.ID, metaUser.PROJECT_NAME)
 		if err != nil {
@@ -42,10 +55,45 @@ func PushProject(w http.ResponseWriter, r *http.Request) {
 			fmt.Println("Project Created Succesfully")
 
 			//Start Initializing a github repo here
+			payload := map[string]interface{}{
+				"name":    metaUser.PROJECT_NAME,
+				"private": false,
+			}
+
+			body, _ := json.Marshal(payload)
+
+			req, _ := http.NewRequest("POST", "https://api.github.com/user/repos", bytes.NewBuffer(body))
+			req.Header.Set("Authorization", "token "+token.GITHUB_TOKEN)
+			req.Header.Set("Accept", "application/vnd.github.v3+json")
+
+			client := &http.Client{}
+			resp, err := client.Do(req)
+			if err != nil {
+				fmt.Println("Error:", err)
+				return
+			}
+			defer resp.Body.Close()
+
+			if resp.StatusCode == 201 {
+				fmt.Println("Repository created successfully!")
+			} else {
+				fmt.Println("Failed to create repository. Status:", resp.Status)
+			}
+
+			var repos struct {
+				Name    string `json:"name"`
+				HTMLURL string `json:"html_url"` // This is the repo link
+			}
+
+			if err := json.NewDecoder(resp.Body).Decode(&repos); err != nil {
+				http.Error(w, "Failed to parse repos", http.StatusInternalServerError)
+				return
+			}
 
 			fmt.Fprintln(w, "success : ", http.StatusOK)
 			fmt.Fprintln(w, "message : Collaboration started successfully for project ", project.Name)
 			fmt.Fprintln(w, "project_id : ", project.ID)
+			fmt.Fprintf(w, "github repo URL: %s", repos.HTMLURL)
 
 		} else {
 			fmt.Println("Project already exists with the name : ", project.Name)
